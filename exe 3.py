6295d209e56bd1856fbf1f550faff29561ba356b
luyyz-hub <luiz.almeida.ferreira@escola.pr.gov.br>
2025-10-02 11:21:14 -0300

    update

---
+++ exe 3.py
@@ -0,0 +1,23 @@
+loja_frutas = {
+    'maça' : {'preço':3.50
+            'quantidade': 10}
+    'banana' : {'preço':3.50
+            'quantidade': 10}
+    'uva' : {'preço':3.50
+            'quantidade': 10}
+}
+
+loja_frutas.update({'limão':{'preco':3.50,'quantidade':10}
+                    'abacaxi':{'preco':3.50,'quantidade':10}
+                    'laranja':{'preco':3.50,'quantidade':10}}),
+print(loja_frutas)
+                
+for k,v in loja_frutas.items()
+    loja_frutas.get(k)['preco']*=1.15
+print(loja_frutas
+
+for k,v in loja_frutas.items()
+    loja_frutas.get(k)['quantidade']*=20
+print(loja_frutas)
+
+loja_frutas.pop('maça')
